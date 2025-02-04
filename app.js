afa8653c13727bb5abbd04a789b3d0f114d71a6c
Ylenia <yleniamighela@gmail.com>
2025-02-04 16:59:00 +0100

    modified path image

--- app.js
+++ app.js
@@ -23,35 +23,35 @@ app.get('/bacheca', (req, res) => {
         {
           title: "Ciambellone soffice",
           content: "Una ricetta semplice per un ciambellone soffice e gustoso.",
-          image: "./img/ciambellone.jpeg",
+          image: "ciambellone.jpeg",
           tags: ["Dolci", "Colazione", "Forno"]
         },
 
         {
           title: "Cracker alla barbabietola",
           content: "Uno snack sano e croccante con un tocco di barbabietola.",
-          image: "./img/cracker_barbabietola.jpeg",
+          image: "cracker_barbabietola.jpeg",
           tags: ["Snack", "Salato", "Barbabietola"]
         },
 
         {
           title: "Pane fritto dolce",
           content: "Un dessert fritto irresistibile, perfetto per ogni occasione.",
-          image: "./img/pane_fritto_dolce.jpeg",
+          image: "pane_fritto_dolce.jpeg",
           tags: ["Dolci", "Fritto", "Tradizione"]
         },
 
         {
           title: "Pasta alla barbabietola",
           content: "Un primo piatto colorato e saporito a base di barbabietola.",
-          image: "./img/pasta_barbabietola.jpeg",
+          image: "pasta_barbabietola.jpeg",
           tags: ["Primi", "Barbabietola", "Cucina Italiana"]
         },
 
         {
           title: "Torta paesana",
           content: "Un dolce della tradizione lombarda ricco di sapore.",
-          image: "./img/torta_paesana.jpeg",
+          image: "torta_paesana.jpeg",
           tags: ["Dolci", "Tradizione", "Lombardia"]
         }
 
